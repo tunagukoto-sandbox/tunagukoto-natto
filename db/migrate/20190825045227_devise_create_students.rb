c1c53e554337f54df2136ad976dc08d40cd820d8
katoatsushi <ka.baseball1997@gmail.com>
2019-10-02 13:45:34 +0900

    aaa

--- db/migrate/20190825045227_devise_create_students.rb
+++ db/migrate/20190825045227_devise_create_students.rb
@@ -4,7 +4,8 @@ class DeviseCreateStudents < ActiveRecord::Migration[5.2]
   def change
     create_table :students do |t|
       ## Database authenticatable
-      t.string :name,               null: false, default: ""
+      t.string :first_name,               null: false, default: ""
+      t.string :last_name,               null: false, default: ""
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
       t.string :major_subject
