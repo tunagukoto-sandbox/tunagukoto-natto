0e658335739c5a9c2e95766bf6cd572043f82f27
katoatsushi <ka.baseball1997@gmail.com>
2019-11-02 17:42:52 +0900

    sdd

--- app/controllers/event_customers_controller.rb
+++ app/controllers/event_customers_controller.rb
@@ -9,7 +9,6 @@ class EventCustomersController < ApplicationController
       @event_customer.student_id = current_student.id
       @event_customer.check = false
     end
-    #binding.pry
   	if @event_customer.save
   		redirect_to root_path
   	else
