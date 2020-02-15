2a90aa3ab4bc1376a5a4acf0715afd51bf114356
katoatsushi <ka.baseball1997@gmail.com>
2020-02-15 16:59:32 +0900

    ddd

--- app/controllers/mini_questions_controller.rb
+++ app/controllers/mini_questions_controller.rb
@@ -1,5 +1,5 @@
 class MiniQuestionsController < ApplicationController
-
+  before_action :send_mini_question, except: [:create]
   def create
 
   	student_group_ids = params["student_group_ids"]["student_group_id"]
@@ -35,8 +35,12 @@ class MiniQuestionsController < ApplicationController
 			year:  Time.now.year,
 			month: Time.now.month
 		)
-		flash[:success] = 'アンケートのご協力ありがとうございました！'
-		redirect_to root_path
+		if m.save
+			flash[:success] = 'アンケートのご協力ありがとうございました！'
+			redirect_to root_path
+		else
+			redirect_to home_questionnaire_path
+		end
   	else
   		flash[:success] = '必須項目を入力してください。'
   		redirect_to home_questionnaire_path
