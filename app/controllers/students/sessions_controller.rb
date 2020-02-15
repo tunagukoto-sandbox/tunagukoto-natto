5b8d0dbdce0e2aac4f2c187b1885af1c9f036254
katoatsushi <ka.baseball1997@gmail.com>
2020-02-15 15:43:28 +0900

    dd

--- app/controllers/students/sessions_controller.rb
+++ app/controllers/students/sessions_controller.rb
@@ -33,7 +33,7 @@ class Students::SessionsController < Devise::SessionsController
   # end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  def configure_sign_in_params
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  end
 end
