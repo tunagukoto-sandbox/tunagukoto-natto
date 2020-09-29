6426796b4db9f2e84b3e1827cdb22bc3e4ab244a
katoatsushi <ka.baseball1997@gmail.com>
2020-09-29 11:00:05 +0900

    dd

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -127,7 +127,7 @@ class HomeController < ApplicationController
 
   def admin_event_csv
     # csv_data = CSV.generate(encoding: Encoding::SJIS, row_sep: "\r\n", force_quotes: true) do |csv|
-    csv_data = CSV.generate(encoding: Encoding::SJIS, row_sep: "\r\n", force_quotes: true) do |csv|
+    csv_data = CSV.generate(row_sep: "\r\n", force_quotes: true) do |csv|
       @event = Event.find(params[:event_id])
       @users = EventCustomer.where(event_id: params[:event_id])
       csv_column_names = ["名前","電話番号","メールアドレス"]
@@ -146,7 +146,7 @@ class HomeController < ApplicationController
 
   def admin_mini_event_csv
     # csv_data = CSV.generate do |csv|
-    csv_data = CSV.generate(encoding: Encoding::SJIS, row_sep: "\r\n", force_quotes: true) do |csv|
+    csv_data = CSV.generate(row_sep: "\r\n", force_quotes: true) do |csv|
       @mini_event = MiniEvent.find(params[:mini_event_id])
       @users = MiniEventCustomer.where(mini_event_id: params[:mini_event_id])
       csv_column_names = ["名前","電話番号","メールアドレス"]
