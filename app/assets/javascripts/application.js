2e2abd100232589b1a2893363bcfea2fbb79580a
katoatsushi <ka.baseball1997@gmail.com>
2020-10-13 22:26:11 +0900

    dd

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require highcharts/highcharts
+//= require highcharts/highcharts-more
+//= require highcharts/highstock
 //= require activestorage
 //= require rails-ujs
 //= require jquery
